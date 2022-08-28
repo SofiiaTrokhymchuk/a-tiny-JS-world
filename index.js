9efcfe06073ae770f06a7c8f25a406b433b1e185
SofiiaTrokhymchuk <s.s.trokhymchuk@gmail.com>
2022-08-29 01:00:49 +0300

    Code refactoring to OOP

--- index.js
+++ index.js
@@ -1,98 +1,83 @@
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
+class Inhabitant{
+   constructor(species, name, gender, saying){
+      this.species = species;
+      this.name = name;
+      this.gender = gender;
+      this.saying = saying;
+      this.properties = ['species', 'name', 'gender', 'saying'];
+   }
 
-   Code repository: https://github.com/SofiiaTrokhymchuk/a-tiny-JS-world
-   Web app: https://sofiiatrokhymchuk.github.io/a-tiny-JS-world/
-   */
+   addFriends(friendsList){
+      this.friends = friendsList;
+      this.properties.push('friends');
+   }
 
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
-const dog = {
-   species: 'dog',
-   name: 'Bima',
-   gender: 'female',
-   legs: 4,
-   hands: 0,
-   saying: 'Bark! ▼・ᴥ・▼'
-};
+   toString(){
+      return this.properties.map(property => {
+         return (Array.isArray(this[property])) ?
+            (`<strong>${property}</strong>: ${this[property].join(", ")}`) :
+            (`<strong>${property}</strong>: ${this[property]}`)
+      }).join(", ");
+   }
+}
 
-const cat = {
-   species: 'cat',
-   name: 'Jessy',
-   gender: 'female',
-   legs: 4,
-   hands: 0,
-   saying: 'Meow! (=^･ω･^=)'
-};
+class Animal extends Inhabitant{
+   constructor(species, name, gender, saying){
+      super(species, name, gender, saying);
+      this.legs = 4;
+      this.properties.push('legs');
+   }
+}
 
-const male = {
-   species: 'human',
-   name: 'Ash Lynx',
-   gender: 'male',
-   legs: 2,
-   hands: 2,
-   saying: 'Stay with me... I won\'t ask "forever." Just for now, Eiji.'
-};
+class Human extends Inhabitant{
+   constructor(name, gender, saying){
+      super('human', name, gender, saying);
+      this.legs = 2;
+      this.hands = 2;
+      this.properties.push('legs', 'hands');
+   }
+}
 
-const female = {
-   species: 'human',
-   name: 'Temari',
-   gender: 'female',
-   legs: 2,
-   hands: 2,
-   saying: 'Amazing. This slacker-clown outsmarted me!'
-};
+class Dog extends Animal{
+   constructor(name, gender){
+      super('dog', name, gender, 'Bark! ▼・ᴥ・▼');
+   }
+}
 
-const catWoman = Object.create(cat);
-catWoman.species = 'cat-woman';
-catWoman.name = 'Hello Kitty';
-catWoman.legs = 2;
-catWoman.hands = 2;
+class Cat extends Animal{
+   constructor(name, gender){
+      super('cat', name, gender, 'Meow! (=^･ω･^=)');
+   }
+}
 
-function inhabitantToString(inhabitant){
-   const friends = inhabitant.friends === undefined ? [0] : inhabitant.friends;
-   return `Inhabitant: ${inhabitant.species}, <strong>${inhabitant.name}</strong>, ${inhabitant.gender}, ${inhabitant.legs}, ` + 
-   `${inhabitant.hands}, <em>${inhabitant.saying}</em>. Friends: ${friends.join(", ")}`;
-};
+class Woman extends Human{
+   constructor(name, saying){
+      super(name, 'female', saying);
+   }
+}
 
-const inhabitants = [dog, cat, male, female, catWoman];
+class Man extends Human{
+   constructor(name, saying){
+      super(name, 'male', saying);
+   }
+}
 
-function addFriends(inhabitant, friendsList){
-   inhabitant.friends = friendsList;
-};
+class CatWoman extends Cat{
+   constructor(name){
+      super(name, 'female');
+      this.species = 'cat-woman';
+      this.legs = 2;
+      this.hands = 2;
+   }
+}
 
-function addFriendsToInhabitants(){
-   addFriends(dog, [cat.name, 'Yasya']);
-   addFriends(female, ['Shikamaru']);
-   addFriends(catWoman, [cat.name])
-};
+const dog = new Dog('Bima', 'female');
+const cat = new Cat('Jessy', 'female');
+const man = new Man('Ash Lynx', 'Stay with me... I won\'t ask "forever." Just for now, Eiji.');
+const woman = new Woman('Temari', 'Amazing. This slacker-clown outsmarted me!');
+const catWoman = new CatWoman('Hello Kitty')
 
-addFriendsToInhabitants();
+dog.addFriends([cat.name, 'Yasya']);
+woman.addFriends(['Shikamaru']);
 
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
-
-  function printInhabitants(inhabitants){
-   inhabitants.forEach(inhabitant => {
-      print(inhabitantToString(inhabitant))
-   });
-  }
-
-  printInhabitants(inhabitants);
-
+[dog, cat, man, woman, catWoman].forEach(inhabitant => print(inhabitant.toString()));
